8504d07bacd6bb9dc016ec93aa54a1c9bdeed62f
adwaith85 <adwaithadhu85227@gmail.com>
2025-10-22 13:08:01 +0530

    Uncomment baseURL for Axios client

--- driver-frontend/src/API/AxiosClient.js
+++ driver-frontend/src/API/AxiosClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://project-uber.onrender.com",
     // baseURL: "https://project-uber.onrender.com",
+    baseURL: "https://project-uber.onrender.com",
 
 })
 
